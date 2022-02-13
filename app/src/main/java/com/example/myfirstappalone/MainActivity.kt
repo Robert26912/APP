d50220453116431891f7ccc7485f92755da23ca3
Robert26912 <70365217+Robert26912@users.noreply.github.com>
2022-02-13 20:14:51 +0200

    Update MainActivity.kt
    change from GitHub to Android studio

--- app/src/main/java/com/example/myfirstappalone/MainActivity.kt
+++ app/src/main/java/com/example/myfirstappalone/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //hello
+        //hello from github
     }
 
 }
